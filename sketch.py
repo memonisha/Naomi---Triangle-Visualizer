6a7f9dacd68910b8deede386f9316d02bbeb8d41
memonisha <75033521+memonisha@users.noreply.github.com>
2024-03-25 15:35:35 +0530

    Add files via upload

--- sketch.py
+++ sketch.py
@@ -26,8 +26,11 @@ def draw():
   
   stroke("white")
   strokeWeight(3)
+  stroke(252,255,3) #yellow
   line(c.x,c.y,a.x,a.y)
+  stroke(108,212,163)  #olive green
   line(b.x,b.y,a.x,a.y)
+  stroke(181,143,227) #purple
   line(b.x,b.y,c.x,c.y)
   noStroke()
   fill("blue")
@@ -36,7 +39,43 @@ def draw():
   b.draw()
   fill("orange")
   c.draw()
+  # a and c -----> (a.x,a.y)    (c.x,c.y)
+  distYellow= distance(a,c) 
+  distGreen= distance(a,b)
+  distPurple= distance(b,c)
+
+
+
+  midPointYellowX= (a.x+c.x)/2
+  midPointYellowY= (a.y+c.y)/2
+  fill("black")
+  rect(midPointYellowX,midPointYellowY,40,20)
+  fill(252,255,3)
+  text(distYellow,midPointYellowX, midPointYellowY)
+
+  midPointGreenX=(a.x+b.x)/2
+  midPointGreenY=(a.y+b.y)/2
+  fill("black")
+  rect(midPointGreenX,midPointGreenY,40,20)
+  fill(108,212,163)
+  text(distGreen,midPointGreenX, midPointGreenY)
+
+  midPointPurpleX=(b.x+c.x)/2
+  midPointPurpleY=(b.y+c.y)/2
+  fill("black")
+  rect(midPointPurpleX,midPointPurpleY,40,20)
+  fill(181,143,227)
+  text(distPurple,midPointPurpleX,midPointPurpleY)
   
-  #circle(200,200,20)
-  #circle(100,150,20)
-  #circle(300,150,20)
+
+ 
+  
+
+#functions with some return value
+def distance(p,q):
+  d=round(sqrt((p.x-q.x)**2 + (p.y-q.y)**2))
+  return d
+
+
+ 
+  
